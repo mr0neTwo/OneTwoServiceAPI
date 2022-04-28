1210a06b1243f2be19d8969eb0ab699b6936cb49
stas <stasmen@nail.ru>
2022-04-28 21:55:52 +0300

    OneTwoOnline 1.37 Add ability resize and order OrderTable

--- init.py
+++ init.py
@@ -4,7 +4,8 @@ from app.db.interaction.interaction import DbInteraction
 from app.db.models.models import Events, Clients
 
 db = DbInteraction(
-    host='5.53.124.252',
+    # host='5.53.124.252',
+    host='localhost',
     port='5432',
     user='postgres',
     password='225567',
@@ -13,4 +14,8 @@ db = DbInteraction(
 )
 
 # Создание новых таблиц
-db.create_tables([Events.__table__])
+# db.create_tables([Events.__table__])
+
+# db.drop_all_tables()
+
+
